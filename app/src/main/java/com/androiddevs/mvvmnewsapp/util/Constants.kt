7e17129f597633b7f08338097645ca3456f71468
Anatame <anata.me97@gmail.com>
2021-11-29 12:41:53 +0530

    added swipe to delete in recylcerview, save and delete article feature.

--- app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -3,6 +3,7 @@ package com.androiddevs.mvvmnewsapp.util
 class Constants {
     companion object {
         const val API_KEY = "6ca036ad239540a580285836b5381902"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
         const val BASE_URL = "https://newsapi.org"
     }
 }
