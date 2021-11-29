9a56bf9a59d9601fcc0bb911fa1aaeb3430a8c51
Anatame <anata.me97@gmail.com>
2021-11-29 08:02:01 +0530

    Added navigation and retrofit instance

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/Article.kt
@@ -0,0 +1,21 @@
+package com.androiddevs.mvvmnewsapp
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.google.gson.annotations.SerializedName
+
+@Entity(
+    tableName = "articles"
+)
+data class Article(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int? = null,
+    val author: String,
+    val content: String,
+    val description: String,
+    val publishedAt: String,
+    val source: Source,
+    val title: String,
+    val url: String,
+    val urlToImage: String
+)
